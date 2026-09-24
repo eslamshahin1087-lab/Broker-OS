a1ffc74d2d2a0333452ed3e8c5971231195abaa2
eslamshahin1087-lab <eslam.shahin1087@gmail.com>
2026-09-24 22:55:18 +0300

    refactor: route logo presentation through Broker-os-USER UI layer

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,1 @@
-import logo from '../assets/logo.png'
-
-export default function Logo({ width = 150, className = '' }) {
-  return (
-    <img
-      className={'app-logo ' + className}
-      src={logo}
-      alt="Broker OS"
-      width={width}
-      style={{ width }}
-    />
-  )
-}
+export { BrokerOSUserLogo as default, BrokerOSUserLogo } from '../ui/Broker-os-USER.jsx'
